df056b6e3136b2cb2e5d5db82697579b7aa24612
Oxana Kozhaeva <kozhaeva_1@mail.ru>
2022-08-18 12:42:23 +0300

    Add task29

---
+++ Task29/Program.cs
@@ -0,0 +1,45 @@
+﻿/*Задача 29: Напишите программу, которая задаёт массив из N элементов и выводит их на экран.
+
+1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+
+6, 1, 33 -> [6, 1, 33]*/
+class Program
+{
+static void WriteArray(int[] array)
+{
+    Console.WriteLine("Вывод массива");
+        for (int i=0;i<array.Length;i++)
+        {
+        Console.Write(array[i]+" ");
+        }
+    Console.WriteLine(" ");
+}
+static void Main(string[]args)
+{
+    int[]array=new int[0];
+
+    while (true)
+    {
+        Console.WriteLine("Введите значение массива");
+        string meaning=Console.ReadLine();
+        if (meaning!="end")
+        {
+            int number=Convert.ToInt32(meaning);
+            int[] newArray=array;
+            array=new int[array.Length+1];
+            for (int i = 0; i < newArray.Length; i++)
+            {
+               array[i]=newArray[i]; 
+            }
+            array[array.Length-1]=number;
+        }
+        else
+        {
+            break;
+        }
+        
+    }
+    WriteArray(array);
+}
+}
+
